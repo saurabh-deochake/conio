c7db19427d84bc1548fae02acb61e086458dc223
Saurabh Deochake <saurabh.d04@gmail.com>
2017-07-18 14:10:30 -0700

    used nested dictionaries for storing the output

--- src/runtime.py
+++ src/runtime.py
@@ -45,36 +45,38 @@ class Runtime:
 		# run Fio based on parameters and number of containers
 		def runTool(self,tool, containerIDs, fioParams, nvmeParams):
 			try:
-				print "\n"
-				with click.progressbar(containerIDs) as bar:
-					for id in bar:
-
-							#if tool is fio tool=1
-							if tool == 1:
-								print "\nRunning Fio inside container:%s"%id
-								cmd = "docker exec "+id+" fio "+fioParams
-								res = subprocess.check_output(cmd, shell=True)
-								print "\nSummary for container:%s"%id
-								print res+"\n\n"
-								self.summarize(tool, res)
+				out = []
+				for id in containerIDs:
+					#if tool is fio tool=1
+					if tool == 1:
+						print "Running Fio inside container:%s"%id
+						cmd = "docker exec "+id+" fio "+fioParams
+						res = subprocess.check_output(cmd, shell=True)
+						#print "\nSummary for container:%s"%id
+					#	print res+"\n\n"
+						out.append(self.process(id, tool, res))
+					#	self.summarize(out)
 							
-							# if tool is nvme tool=2
-							elif tool == 2:
-								print "\nRunning NVMe-CLI inside container:%s"%id
-								cmd = "docker exec "+id+" nvme "+nvmeParams
-								res = subprocess.check_output(cmd, shell=True)
-								print "\nSummary for container:%s"%id
-								self.summarize(tool, res)
-							else:
-								## Run both here parallel
-								print "\nRunning Fio and NVMe-CLI inside container:%s"%id
-								cmd = "docker exec "+id+" fio "+fioParams+\
+					# if tool is nvme tool=2
+					elif tool == 2:
+						print "Running NVMe-CLI inside container:%s"%id
+						cmd = "docker exec "+id+" nvme "+nvmeParams
+						res = subprocess.check_output(cmd, shell=True)
+						#print "\nSummary for container:%s"%id
+						out.append(self.process(id, tool, res))
+					#	self.summarize(out)
+					else:
+						## Run both here parallel
+						print "Running Fio and NVMe-CLI inside container:%s"%id
+						cmd = "docker exec "+id+" fio "+fioParams+\
 									  " & nvme "+nvmeParams
-								res = subprocess.check_output(cmd, shell=True)
-								print res+"\n\n"
+						res = subprocess.check_output(cmd, shell=True)
+						#print res+"\n\n"
 								
-								print "\nSummary for container:%s"%id
-								self.summarize(tool, res)
+						#print "\nSummary for container:%s"%id
+						out.append(self.process(id, tool, res))
+								
+				self.summarize(out)
 
 			except Exception, e:
 					print "\n[ERROR] Something went wrong. Try again!"
@@ -82,49 +84,62 @@ class Runtime:
 
 ## -------------------------------------------------------------------------
 
-		# print the output on screen
-		def summarize(self, tool, res):
+		# Process the output and store it in dictionary
+		def process(self, id, tool, res):
 			try:
+				output = {}
+				output[id] = {}
 				if tool == 1:
 					lines = res.split("\n")
 					op = res.split("Starting")[1].split("\n")
-					print "\nFIO I/O benchmark:"
-					print "> Operation:"+lines[0].split(",")[0].split(":")[2].split("=")[1]
-					print "> Jobs:"+op[2].split(" ")[2].split("=")[1].split(")")[0]
-					print "> Blocksize:"+lines[0].split(",")[1].split("=")[1].split("-")[0]
-					print "> Iodepth:"+lines[0].split(",")[3].split("=")[1]
-					print "> IOPS:"+op[3].split(",")[2].split("=")[1]
-					print "> Bandwidth:"+op[3].split(",")[1].split("=")[1]
-					print "> Avg. Latency:"+op[5].split(",")[2].split("=")[1]+" usec"
-					print "> 99.99 Latency:"+op[12].split(" ")[-1][:-1]+" usec"
+					output[id]['Operation'] = lines[0].split(",")[0].split(":")[2].split("=")[1]
+					output[id]['Jobs'] = op[2].split(" ")[2].split("=")[1].split(")")[0]
+					output[id]['Blocksize'] = lines[0].split(",")[1].split("=")[1].split("-")[0]
+					output[id]['Iodepth'] = lines[0].split(",")[3].split("=")[1]
+					output[id]['IOPS'] = op[3].split(",")[2].split("=")[1]
+					output[id]['Bandwidth'] = op[3].split(",")[1].split("=")[1]
+					output[id]['Avg Latency'] = op[5].split(",")[2].split("=")[1]+" usec"
+					output[id]['99.99 Latency'] = op[12].split(" ")[-1][:-1]+" usec"
 				if tool == 2:
 					lines = res.split("\n")
-					print "\nNVMe-CLI:"
-					print "> Temperature:"+lines[2].split(":")[1]
-					print "> Available Spare:"+lines[3].split(":")[1]
-					print "> Percent Used:"+lines[5].split(":")[1]
-					print "> Data Units Read:"+lines[6].split(":")[1]
-					print "> Data Units Written:"+lines[7].split(":")[1]
+					output[id]['Temperature'] = lines[2].split(":")[1]
+					output[id]['Available Spare'] = lines[3].split(":")[1]
+					output[id]['Percent Used'] = lines[5].split(":")[1]
+					output[id]['Data Units Read'] = lines[6].split(":")[1]
+					output[id]['Data Units Written'] = lines[7].split(":")[1]
 				if tool == 3:
 					lines = res.split("\n")
 					op = res.split("Starting")[1].split("\n")
-					print "\nFIO I/O benchmark:"
-					print "> Operation:"+lines[18].split(",")[0].split(":")[2].split("=")[1]
-					print "> Jobs:"+op[2].split(" ")[2].split("=")[1].split(")")[0]
-					print "> Blocksize:"+lines[18].split(",")[1].split("=")[1].split("-")[0]
-					print "> Iodepth:"+lines[18].split(",")[3].split("=")[1]
-					print "> IOPS:"+op[3].split(",")[2].split("=")[1]
-					print "> Bandwidth:"+op[3].split(",")[1].split("=")[1]
-					print "> Avg. Latency:"+op[5].split(",")[2].split("=")[1]+" usec"
-					print "> 99.99 Latency:"+op[12].split(" ")[-1][:-1]+" usec"
+					output[id]['Operation'] = lines[18].split(",")[0].split(":")[2].split("=")[1]
+					output[id]['Jobs'] = op[2].split(" ")[2].split("=")[1].split(")")[0]
+					output[id]['Blocksize'] = lines[18].split(",")[1].split("=")[1].split("-")[0]
+					output[id]['Iodepth'] = lines[18].split(",")[3].split("=")[1]
+					output[id]['IOPS'] = op[3].split(",")[2].split("=")[1]
+					output[id]['Bandwidth'] = op[3].split(",")[1].split("=")[1]
+					output[id]['Avg Latency'] = op[5].split(",")[2].split("=")[1]+" usec"
+					output[id]['99.99 Latency'] = op[12].split(" ")[-1][:-1]+" usec"
 
-					# NVME
-					print "\nNVMe-CLI:"
-					print "> Temperature: "+lines[2].split(":")[1]
-					print "> Available Spare: "+lines[3].split(":")[1]
-					print "> Percent Used: "+lines[5].split(":")[1]
-					print "> Data Units Read: "+lines[6].split(":")[1]
-					print "> Data Units Written: "+lines[7].split(":")[1]
+					#NVME
+					output[id]['Temperature'] = lines[2].split(":")[1]
+					output[id]['Available Spare'] = lines[3].split(":")[1]
+					output[id]['Percent Used'] = lines[5].split(":")[1]
+					output[id]['Data Units Read'] = lines[6].split(":")[1]
+					output[id]['Data Units Written'] = lines[7].split(":")[1]
+				
+				return output
+			except Exception, e:
+					print "\n[ERROR] Something went wrong. Try again!"
+					print str(e)
+## -------------------------------------------------------------------------
+
+		# print the output on screen
+		def summarize(self, output):
+			try:
+				for output in output:
+					for key  in output:
+						print "\n---Summary for container:%s---"%key
+						for innerkey in output[key]:
+							print innerkey+"="+output[key][innerkey]
 
 			except Exception, e:
 					print "\n[ERROR] Something went wrong. Try again!"
