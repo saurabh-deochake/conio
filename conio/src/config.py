d43a9532b10ffb2bcb23847be118b8c6cc63cebf
Saurabh Deochake <saurabh.d04@gmail.com>
2017-08-14 15:33:33 -0700

    observe pylint

--- conio/src/config.py
+++ conio/src/config.py
@@ -6,9 +6,9 @@ Copyright 2017 Intel Corporation
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed 
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 	
 Author: Saurabh Deochake, Intel Corporation
@@ -16,7 +16,7 @@ Author: Saurabh Deochake, Intel Corporation
 
 ## File which stores all global variables
 
-## ----------------IMPORTANT NOTE------------------------## 
+## ----------------IMPORTANT NOTE------------------------##
 ## Please leave a space after you define your variable.  ##
 ## Failure to do so may cause fatal misbehavior.         ## 
 ##                Proceed with caution!                  ##
