32ee3108968ec46534cef9021e536b4bb9ba8c0f
Saurabh Deochake <saurabh.d04@gmail.com>
2017-08-14 16:14:09 -0700

    modified to pep8

--- setup.py
+++ setup.py
@@ -8,35 +8,35 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
 http://www.apache.org/licenses/LICENSE-2.0
-	
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-	
+
 Author: Saurabh Deochake, Intel Corporation
 """
 
 from setuptools import setup
 
-
+DESCRIPTION = 'Conio- A lightweight tool for containerized I/O benchmarking \
+of NVMe-based cloud storage'
 with open('README.md') as f:
-	long_description = f.read()
+    LONG_DESCRIPTION = f.read()
 
 setup(
-	name='conio',
-	version='1.0.4',
-	description='Conio- A lightweight tool for containerized I/O benchmarking of NVMe-based cloud storage',
-	long_description = long_description,
-	url = 'https://github.com/saurabh-deochake/conio',
-	author='Saurabh Deochake',
-	author_email='saurabh.deochake@intel.com',
-	license='Apache License, Version 2.0',
-	install_requires=['Click','configparser','ascii_graph'],
-	entry_points='''
-		[console_scripts]
-		conio=conio.src.conio:conio
-	'''
-
+    name='conio',
+    version='1.0.4',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/saurabh-deochake/conio',
+    author='Saurabh Deochake',
+    author_email='saurabh.deochake@intel.com',
+    license='Apache License, Version 2.0',
+    install_requires=['Click', 'configparser', 'ascii_graph'],
+    entry_points='''
+          [console_scripts]
+          conio=conio.src.conio:conio
+    '''
 )
